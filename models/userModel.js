7216133a424b56d3783f535729bfc4a84af58de7
davitGarsia <davitgarsiashvili@gmail.com>
2023-08-24 19:12:30 +0400

    create: user model

--- models/userModel.js
+++ models/userModel.js
@@ -1,1 +1,44 @@
 const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please, tell us your name'],
+  },
+
+  lastName: {
+    type: String,
+    required: [true, 'Please, tell us your last name'],
+  },
+
+  userName: {
+    type: String,
+    required: [true, 'Please enter your username'],
+    unique: true,
+    lowercase: true,
+    validate: [
+      validator.isEmail,
+      'Please, provide a valid email for your username',
+    ],
+  },
+  password: {
+    type: String,
+    required: [true, 'Please enter a password'],
+    minlength: 8,
+    //select: false,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please, confirm password'],
+    validate: {
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'Passwords are not the same!',
+    },
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
